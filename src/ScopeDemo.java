fd5d66475dc7278a076b49320b3daeed02eeae3a
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-17 13:04:50 -0500

    variable initiation demo

--- src/ScopeDemo.java
+++ src/ScopeDemo.java
@@ -5,10 +5,17 @@ public class ScopeDemo {
     x = 10;
     if (true) {
       int y = 20;
-      System.out.println("x and y:" + x + " " + y);
+      System.out.println("x and y: " + x + " " + y);
       x = y * 2;
       System.out.println(("x:" + x));
     }
     System.out.println("x is still:" + x);
+
+    for (x = 0; x < 3; x++) {
+      int y = 20;
+      System.out.println("y is:" + y);
+      y= 100;
+      System.out.println("y is now:" + y);
+    }
   }
 }
